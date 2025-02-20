2e1cb23a064068faa335f9d2ba2bcf45a51e17a3
Fer <fervel1704@gmail.com>
2025-02-20 04:28:21 -0400

    entrypoint fix

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -1,51 +1,48 @@
 #!/bin/sh
 
-if [ ! -f "./.docker/setup_done" ]; then
-        # Navigate to the Laravel application directory
+if [ ! -f "/var/www/.docker/.setup_done" ]; then
+    # Navigate to the Laravel application directory
     cd /var/www
 
     # Check if the .env file exists, if not, copy from .env.example
     if [ ! -f ".env" ]; then
         echo "Copiando .env.example a .env"
-        cp .env.example .env
+        cp .env.example .env || exit 1
     fi
 
     # Update database configuration in .env file
     echo "Actualizando la configuración de la BD..."
-
-    sed -i 's/^DB_CONNECTION=.*/DB_CONNECTION=pgsql/' .env
-    sed -i 's/^# \?DB_HOST=.*/DB_HOST=postgres/' .env
-    sed -i 's/^# \?DB_PORT=.*/DB_PORT=5432/' .env
-    sed -i 's/^# \?DB_DATABASE=.*/DB_DATABASE=mycloud/' .env
-    sed -i 's/^# \?DB_USERNAME=.*/DB_USERNAME=mycloud/' .env
-    sed -i 's/^# \?DB_PASSWORD=.*/DB_PASSWORD=mycloud123/' .env
+    sed -i 's/^DB_CONNECTION=.*/DB_CONNECTION=pgsql/' .env &&
+    sed -i 's/^# \?DB_HOST=.*/DB_HOST=postgres/' .env &&
+    sed -i 's/^# \?DB_PORT=.*/DB_PORT=5432/' .env &&
+    sed -i 's/^# \?DB_DATABASE=.*/DB_DATABASE=mycloud/' .env &&
+    sed -i 's/^# \?DB_USERNAME=.*/DB_USERNAME=mycloud/' .env &&
+    sed -i 's/^# \?DB_PASSWORD=.*/DB_PASSWORD=mycloud123/' .env || exit 1
 
     # Install composer dependencies
-    # if [ ! -d "vendor" ]; then
     echo "Instalando dependencias de Composer..."
-    composer install
-    # fi
+    composer install || exit 1
 
     # Generate application key
     if [ ! -f "storage/oauth-private.key" ]; then
         echo "Generando application key..."
-        php artisan key:generate
+        php artisan key:generate || exit 1
     fi
 
     # Run migrations
     echo "Ejecutando migrations..."
-    php artisan migrate --force
+    php artisan migrate --force || exit 1
 
     # Install Node.js dependencies
     if [ -f "package.json" ]; then
         echo "Instalando dependencias de Node.js..."
-        npm install
+        npm install || exit 1
     fi
 
-    # Create flag to indicate setup is done
-    touch ./.docker/.setup_done
-
-    echo "Configuración inicial completada."
+    # Create flag to indicate setup is done, only if all previous commands succeeded
+    touch ./.docker/.setup_done && echo "===>>> CONFIGURACIÓN INICIAL COMPLETADA."
+else
+    echo "===>>> CONFIGURACIÓN INICIAL OMITIDA."
 fi
 
 # Run the original PHP-FPM command
